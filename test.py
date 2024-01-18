4da3d1f7117ce86e8eb951d09f0e6d15cad124c5
yl <2317676993@qq.com>
2024-01-18 21:59:32 +0800

    tinyllava

--- test.py
+++ test.py
@@ -12,12 +12,12 @@ import requests
 from PIL import Image
 
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration
+from transformers import AutoProcessor, LlavaForConditionalGeneration, AutoTokenizer
 
 model_id = "checkpoints/tinyllava-v1.0-1.1b-hf"
 
-prompt = "USER: <image>\nWhat are these?\nASSISTANT:"
-image_file = "images/llava_logo.png"
+# prompt = "USER: <image>\nWhat are these?\nASSISTANT:"
+# image_file = "images/llava_logo.png"
 
 model = LlavaForConditionalGeneration.from_pretrained(
     model_id, 
@@ -28,8 +28,23 @@ model = LlavaForConditionalGeneration.from_pretrained(
 processor = AutoProcessor.from_pretrained(model_id)
 
 # raw_image = Image.open(requests.get(image_file, stream=True).raw)
-raw_image = Image.open(image_file)
-inputs = processor(prompt, raw_image, return_tensors='pt').to('cuda', torch.float16)
+# raw_image = Image.open(image_file)
+# inputs = processor(prompt, raw_image, return_tensors='pt').to('cuda', torch.float16)
 
-output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
-print(processor.decode(output[0][2:], skip_special_tokens=True))
+# output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+# print(processor.decode(output[0][2:], skip_special_tokens=True))
+
+model_max_length = 512
+tokenizer = AutoTokenizer.from_pretrained(
+        model_id,
+        cache_dir=None,
+        model_max_length=model_max_length,
+        padding_side="right",
+        use_fast=True,
+    )
+# tokenizer.pad_token = tokenizer.unk_token
+print(f'pad_token:{tokenizer.pad_token}')
+print(f'unk_token:{tokenizer.unk_token}')
+num_added_tokens = tokenizer.add_tokens("[SEG]")
+seg_token_idx = tokenizer("[SEG]", add_special_tokens=False).input_ids[0]
+print(f'seg_token_idx:{seg_token_idx}')
