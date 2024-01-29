4618a9c7cd725dfcfc2b9879ec2e7e1913c800ee
yl <2317676993@qq.com>
2024-01-29 18:59:08 +0800

    opt-llava

--- llava/model/language_model/llava_opt.py
+++ llava/model/language_model/llava_opt.py
@@ -45,7 +45,7 @@ class LlavaOPTForCausalLM(OPTForCausalLM, LlavaMetaForCausalLM):
     def __init__(self, config):
         super(LlavaOPTForCausalLM, self).__init__(config)
         self.model = LlavaOPTModel(config)
-        self.pretraining_tp = config.pretraining_tp
+        # self.pretraining_tp = config.pretraining_tp
         self.vocab_size = config.vocab_size
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
